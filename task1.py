8aaaa4ec28d99b7a92096b8ecc48594da43a8ccd
ssaahhil832 <147607338+ssaahhil832@users.noreply.github.com>
2025-05-17 22:12:57 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+#taking two inputs from user and printing them
+
+a = float(input("enter a value :")) 
+b= float(input("enter b value :"))
+
+
+#performing operations on the inputs
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+#displaying the results
+print("sum of a and b is : ", sum)
+print("subtraction of a and b is : ", sub)
+print("multiplication of a and b is : ", mul)
+print("division of a and b is : ", div)
+
