27a6c7f2ed66d98719dac2cd546a672cb1b8856d
ssaahhil832 <147607338+ssaahhil832@users.noreply.github.com>
2025-05-18 10:50:29 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,19 @@
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-name = first_name + " " + last_name
-print("Hello, " + name + "! Welcome to the python program.")
+#task 2.1
+#even or odd number
+a = int(input ("enter a number :"))
+if(a%2==0):
+    print( a , "is even  number ")
+else:
+    print ( a ,"is odd number ")    
+print('end')
+
+
+#task 2.2
+n=int(input("enter a number :"))
+if n<0:
+    print("please enter a positive number")
+sum=0
+for i in range(1,n):
+    sum=sum+i
+print("sum of first ",n," numbers is : ", sum)
+print('end')    
