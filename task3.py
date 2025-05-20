3e9ae9c885392db7b8077648e3bdc04a40cc5c05
ssaahhil832 <147607338+ssaahhil832@users.noreply.github.com>
2025-05-20 18:14:09 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,11 +14,12 @@ import math
 def calculate_square_root(number):
     if number < 0:
         raise ValueError("Cannot calculate square root of a negative number")
-    return math.sqrt(number)
-
-num= int(input("Enter a number: "))
-try:
-    result = calculate_square_root(num)
-    print(f"The square root of {num} is {result}")
-except ValueError as e:
-    print(e)
+    a=math.sqrt(number)
+    print('square root of',num,'is', a)
+    b=math.log(number)
+    print('log of',num,'is', b)
+    c = math.sin(math.radians(number))
+    print('sin of',num,'is', c)
+    
+num=int(input("Enter a number :"))
+calculate_square_root(num)
